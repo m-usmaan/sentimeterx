af87aa0c7aff1cf67a2a59dc58385ed94d7aae3e
Usmaan <muhammad.usman02@arbisoft.com>
2024-12-05 22:15:26 +0500

    Added navigation to new chat button

--- src/components/common/sidebar/index.js
+++ src/components/common/sidebar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import {
   SidebarContainer,
@@ -13,31 +14,31 @@ import HamburgerIcon from "assets/icons/hamburger.svg";
 import { default as Link } from "components/common/IsActiveNavLink";
 import ROUTES from "constants/routes";
 import { AddIcon } from "assets/SVGs";
+import { HOME_URL } from "constants/urls";
 
 function Sidebar() {
+  const navigate = useNavigate();
   return (
     <SidebarContainer>
       <TopContainer>
         <LogoImage loading="lazy" src={HamburgerIcon} alt="hamburger" />
-        <NewChatButton>
+        <NewChatButton onClick={() => navigate(HOME_URL)}>
           <AddIcon />
           <span>New Chat</span>
         </NewChatButton>
       </TopContainer>
       <Divider />
       <MenuContainer>
-        {
-          ROUTES.filter((route) => route.index).map(({ Icon, path, text }) => {
-            return (
-              <Link to={path} key={path}>
-                <MenuItem>
-                  <Icon />
-                  <span>{text}</span>
-                </MenuItem>
-              </Link>
-            );
-          })
-        }
+        {ROUTES.filter((route) => route.index).map(({ Icon, path, text }) => {
+          return (
+            <Link to={path} key={path}>
+              <MenuItem>
+                <Icon />
+                <span>{text}</span>
+              </MenuItem>
+            </Link>
+          );
+        })}
       </MenuContainer>
     </SidebarContainer>
   );
