fe59754a0c9167d7a0e9c4d207421abc169a6765
Usmaan <muhammad.usman02@arbisoft.com>
2024-12-05 21:31:32 +0500

    Enhanced Error Page

--- src/components/common/ErrorPage/index.js
+++ src/components/common/ErrorPage/index.js
@@ -1,16 +1,22 @@
-import { useRouteError } from "react-router-dom";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
+import { HOME_URL } from "constants/urls";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
+  const navigate = useNavigate();
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => navigate(HOME_URL)}>
+            Back Home
+          </Button>
+        }
+      />
     </div>
   );
 }
