40f4c7bb1368cc8faec839b98db62ff325da4559
Usmaan <muhammad.usman02@arbisoft.com>
2025-01-10 20:00:22 +0500

    Added feedback quotes design

--- src/components/chatbot/ChatDetail/Feedback Quotes/index.js
+++ src/components/chatbot/ChatDetail/Feedback Quotes/index.js
@@ -1,8 +1,9 @@
-import { Empty, Spin } from "antd";
+import { Avatar, List, Empty, Spin } from "antd";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 import { fetchChatFeedbackQuotes } from "components/chatbot/apis";
+import { QuotesContainer } from "./styles";
 
 const ChatFeedbackQuotes = ({ data, unique_uuid }) => {
   const [quotes, setQuotes] = useState(data);
@@ -29,7 +30,44 @@ const ChatFeedbackQuotes = ({ data, unique_uuid }) => {
     /* eslint-disable-next-line */
   }, []);
 
-  return loading ? <Spin /> : !quotes ? <Empty /> : <div>{quotes}</div>;
+  return loading ? (
+    <Spin />
+  ) : !quotes ? (
+    <Empty />
+  ) : (
+    <QuotesContainer>
+      <strong id="headline">{quotes.query}</strong>
+      <List
+        style={{
+          backgroundColor: "white",
+          borderRadius: "10px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+          padding: "5px 20px 5px 20px",
+          maxHeight: "500px",
+          overflow: "auto"
+        }}
+        pagination={{
+          position: "top",
+          align: "end",
+          pageSize: 5
+        }}
+        dataSource={quotes.reviews}
+        renderItem={(item, index) => (
+          <List.Item key={index} extra={<pre style={{color: "gray"}}>{item.timestamp}</pre>}>
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                />
+              }
+              title={item.customer_id}
+              description={item.text}
+            />
+          </List.Item>
+        )}
+      />
+    </QuotesContainer>
+  );
 };
 
 export default ChatFeedbackQuotes;
---
+++ src/components/chatbot/ChatDetail/Feedback Quotes/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const QuotesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  #headline {
+    font-size: 18px;
+    color: #353535;
+    margin-bottom: 2rem;
+  }
+`;
