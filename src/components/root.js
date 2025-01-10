d60fe5135e987154b79c621b8ba3888f62cf9d97
Usmaan <muhammad.usman02@arbisoft.com>
2025-01-10 20:42:41 +0500

    Started and added base layout

--- src/components/root.js
+++ src/components/root.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import React from "react";
+// import { Outlet } from "react-router-dom";
 
-import { MainContentContainer, OutletContainer } from './styles';
-import Sidebar from 'components/common/Sidebar';
-import Header from 'components/common/Header';
+// import { MainContentContainer, OutletContainer } from './styles';
+// import Sidebar from 'components/common/Sidebar';
+// import Header from 'components/common/Header';
 
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#0958d9",
+};
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "pink",
+};
+const layoutStyle = {
+  overflow: "hidden",
+  width: "100%",
+  height: "100vh",
+};
 
 function MainContent() {
+  const { Header, Sider, Content } = Layout;
+
   return (
-    <MainContentContainer>
-      <Sidebar />
-      <Header />
-      <OutletContainer>
-        <Outlet />
-      </OutletContainer>
-    </MainContentContainer>
+    <Layout style={layoutStyle}>
+      <Sider width="20%" style={siderStyle}>
+        Sider
+      </Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+    </Layout>
   );
 }
 
