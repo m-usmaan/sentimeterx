b8b8097d87157543e5ab2d3176a57a13a973f842
Usmaan <muhammad.usman02@arbisoft.com>
2025-01-16 11:12:25 +0500

    Added icons and colors setting in suggestion card

--- src/components/chatbot/NewChat/ChatSuggestions/index.js
+++ src/components/chatbot/NewChat/ChatSuggestions/index.js
@@ -13,6 +13,35 @@ import {
 } from "./styles";
 import { chatSuggestions, createChat } from "components/chatbot/apis";
 
+const category_assets = {
+  nps: [
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/bbd32c71ac35d98788b35dd131649c93d1a13370ceaa9759f6e2890e3c6b516c?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#fcbb1b",
+    },
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/dedfcd2f3d29322b13ba10965d3d1a9e2179eeebe2f77b066c7120707dd6b4a6?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#62a5e5",
+    },
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/1b6e5937377ab84e90fcf050815a11874d677db299045360a59fb1a6b3c2372a?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#e20015",
+    },
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/fcedda5c82013b5fffdcc2db8c10f1cf032331159ca55f266c186b3fe45d501b?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#57439e",
+    },
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/bbd32c71ac35d98788b35dd131649c93d1a13370ceaa9759f6e2890e3c6b516c?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#fcbb1b",
+    },
+    {
+      icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/1b6e5937377ab84e90fcf050815a11874d677db299045360a59fb1a6b3c2372a?placeholderIfAbsent=true&apiKey=c1c84e4685234ce282d9bb2fac8cee71",
+      color: "#e20015",
+    },
+  ],
+};
+
 function ChatSuggestions() {
   const navigate = useNavigate();
 
@@ -48,29 +77,40 @@ function ChatSuggestions() {
   };
 
   return (
-    <div style={{flexGrow: 1, display: "flex", flexDirection: "column", overflow: "hidden"}}>
+    <div
+      style={{
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+        overflow: "hidden",
+      }}
+    >
       <SuggestionsContainer wrap>
         {loading ? (
           <Spin style={{ margin: "auto" }} />
         ) : (
-          suggestions.map((suggestion) => (
-            <SuggestionCard
-              key={suggestion.id}
-              onClick={() => handleCreate(suggestion.text)}
-            >
-              <SuggestionIcon
-                loading="lazy"
-                src={suggestion.icon}
-                alt={suggestion.category}
-              />
-              <SuggestionContent>
-                <SuggestionTitle style={{ color: suggestion.color }}>
-                  {suggestion.label}
-                </SuggestionTitle>
-                <SuggestionText>{suggestion.text}</SuggestionText>
-              </SuggestionContent>
-            </SuggestionCard>
-          ))
+          suggestions.map((suggestion, index) => {
+            const assets = category_assets[suggestion.category];
+            const asset = assets[index % assets.length];
+            return (
+              <SuggestionCard
+                key={suggestion.id}
+                onClick={() => handleCreate(suggestion.text)}
+              >
+                <SuggestionIcon
+                  loading="lazy"
+                  src={asset.icon}
+                  alt={suggestion.category}
+                />
+                <SuggestionContent>
+                  <SuggestionTitle style={{ color: asset.color }}>
+                    {suggestion.label}
+                  </SuggestionTitle>
+                  <SuggestionText>{suggestion.text}</SuggestionText>
+                </SuggestionContent>
+              </SuggestionCard>
+            );
+          })
         )}
       </SuggestionsContainer>
     </div>
